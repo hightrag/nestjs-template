ab8bca2fab5ea993e289a6ff89db6fc5ce21f69f
ht <hightrag@proton.me>
2024-10-26 16:45:17 +0200

    feat: env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [ConfigModule.forRoot(), ScheduleModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
